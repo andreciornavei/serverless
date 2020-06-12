8e786695996493c6dbe1d17d3a0d5912b68e41a3
Andre Ciornavei <andre.ciornavei@gmail.com>
2020-06-12 01:48:11 -0300

    Allow Log registration for register lambda function

--- terraform/infra/users/iam-policy-register.tf
+++ terraform/infra/users/iam-policy-register.tf
@@ -12,7 +12,16 @@ resource "aws_iam_policy" "register_policy" {
       ],
       "Effect": "Allow",
       "Resource": "${aws_dynamodb_table.users.arn}"
-    }
+    },
+     {
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:PutLogEvents"
+            ],
+            "Resource": "*"
+        }
   ]
 }
 EOF
