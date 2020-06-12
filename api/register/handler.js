b618f8849a22318a66784262e4b035e5af4ed509
Andre Ciornavei <andre.ciornavei@gmail.com>
2020-06-11 23:50:58 -0300

    The first setup of terraform infra and serverless functions

---
+++ api/register/handler.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports.register = async event => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: process.env.DYNAMODB_USERS,
+      }
+    ),
+  };
+};
