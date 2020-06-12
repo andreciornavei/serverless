3c992fdd2faba49360782fbeec7b4f7b627a0796
Andre Ciornavei <andre.ciornavei@gmail.com>
2020-06-12 00:36:25 -0300

    Added iam roles to lambda functions access dynamodb user table

---
+++ terraform/infra/users/iam-policy-register.tf
@@ -0,0 +1,19 @@
+resource "aws_iam_policy" "register_policy" {
+  name        = "${var.environment}-register-policy"
+  description = "This policy allows data insertion on users dynamodb table"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "dynamodb:PutItem"
+      ],
+      "Effect": "Allow",
+      "Resource": "${aws_dynamodb_table.users.arn}"
+    }
+  ]
+}
+EOF
+}
